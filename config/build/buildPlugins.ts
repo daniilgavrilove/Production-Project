98a1a1f43e6ef529ea7279b71d3bba0511ef3819
daniilgavrilove <daniilgavrilove@gmail.com>
2023-09-09 16:09:51 +0300

    fix sb

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -13,6 +13,7 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
         new MiniCssExtractPlugin({
             filename: 'css/[name].[contenthash:8].css',
             chunkFilename: 'css/[name].[contenthash:8].css',
+
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,5 +1,4 @@
 import webpack from 'webpack';
-import path from 'path';
 import { BuildOptions } from './types/config';
 import { buildPlugins } from './buildPlugins';
 import { buildLoaders } from './buildLoaders';
@@ -24,5 +23,6 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
         resolve: buildResolvers(options),
         devtool: isDev ? 'inline-source-map' : undefined,
         devServer: isDev ? buildDevServer(options) : undefined,
+
     };
 }
